29e4751bb5f5fc64d8c977355962d0fbe879cf1e
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-05-05 01:03:48 +0300

    modifications to trainee and course

--- Day01/course/views.py
+++ Day01/course/views.py
@@ -1,14 +1,14 @@
-from django.http.response import HttpResponse
+from django.shortcuts import HttpResponse
 
 # Create your views here.
-def traineelist(request):
-    return HttpResponse('<h1>Trainee List</h1>')
+def courselist(request):
+    return HttpResponse('<h1>Trainee course</h1>')
 
-def addTrainee(request):
-    return HttpResponse('<h1>Add Trainee</h1>')
+def addCourse(request):
+    return HttpResponse('<h1>Add course</h1>')
 
-def updateTrainee(request):
-    return HttpResponse('<h1>Update Trainee</h1>')
+def updateCourse(request):
+    return HttpResponse('<h1>Update course</h1>')
 
-def deleteTrainee(request):
-    return HttpResponse('<h1>Delete Trainee</h1>')
+def deleteCourse(request):
+    return HttpResponse('<h1>Delete course</h1>')
