d7cede3acf3fabfbdfeb3e2de78336739c44fb86
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-05-05 01:59:26 +0300

    structure refactored

--- Day01/trainee/urls.py
+++ Day01/trainee/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from trainee.views import *
 
 urlpatterns = [
-    path('traineelist/', traineelist, name='traineelist'),
+    path('trainees/', traineelist, name='traineelist'),
     path('add/', addTrainee, name='addTrainee'),
-    path('update', updateTrainee, name='updateTrainee'),
-    path('delete', deleteTrainee, name='deleteTrainee'),
+    path('update/', updateTrainee, name='updateTrainee'),
+    path('delete/', deleteTrainee, name='deleteTrainee'),
 ]
