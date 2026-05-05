3ed84267f500938ffdf2667c79f3a6b8821a1a30
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-05-05 04:46:53 +0300

    login and register done

--- Day01/ITIan/settings.py
+++ Day01/ITIan/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #new apps
     'trainee.apps.TraineeConfig',
-    'course.apps.CourseConfig'
+    'course.apps.CourseConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
---
+++ Day01/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'users/base.html')
+
+def login(request):
+    return render(request, 'users/login.html')
+
+def register(request):
+    return render(request, 'users/register.html')
